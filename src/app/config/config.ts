34a2def5d6e99859c2e291eccd2bd628154d8482
Anderson Benicio <andyrodrigo2@gmail.com>
2023-05-02 10:08:07 -0300

    pequeno Teste

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -5,5 +5,6 @@ interface Config {
 export const config: Config = {
   apiUrl: 'https://apimd.topdownrn.com.br',
   page: 'https://TopDown-RN.github.io/pub-movideskspa',
+  //apiUrl: 'https://apimd.topdownrn.com.br',
   //page: 'http://localhost:4200/',
 };
